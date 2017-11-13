74339bf0d7f253239b68f17bb1fab8d0a7d60b06
Rafael Fontoura <rafael.fontoura@dataeasy.com.br>
2017-11-13 09:22:00 -0200

    Exibição de resultados na tela de notícias a partir de query
    1. Utilização de  EJS para retornar query no MySQL para a tela e exibir resultados em tabela
    2. Externalizando configuração com banco de dados

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,18 +1,14 @@
+var dbConnection = require('../../config/dbConnection');
+
 module.exports = function (app) {
 
+    var connection = dbConnection();
     app.get('/noticias', function (req, res) {
-        var mysql = require('mysql');
-        var connection = mysql.createConnection({
-            host: 'localhost',
-            user: 'root',
-            password: 'mysql',
-            database: 'portal_noticias'
-        });
-
+        
         connection.query('SELECT * FROM noticias', function (error, result) {
-            res.send(result);
+            // no render(), o primeiro parâmetro é a view que vai renderizar o resultado
+            // o segundo é a criação de um objeto que será passado para a view
+            res.render('noticias/noticias', { noticias: result });
         });
-
-        //res.render("noticias/noticias")
     });
 }
