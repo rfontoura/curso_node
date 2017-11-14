3d206b66b5f1de3606382c1693bfa1fb90b9d334
Rafael Fontoura <rafael.fontoura@dataeasy.com.br>
2017-11-14 17:10:04 -0200

    Criação de tela de inclusão de notícia
    1. Criação de tela de inclusão de notícia
    2. Inclusão de módulo 'body-parser' para pegar atributos do form e transformar em JSON

--- app/models/noticiasModel.js
+++ app/models/noticiasModel.js
@@ -3,8 +3,12 @@ module.exports = function () {
         connection.query('SELECT * FROM noticias', callback);
     };
 
-    this.getNoticia = function(connection, callback) {
+    this.getNoticia = function (connection, callback) {
         connection.query('SELECT * FROM noticias where id_noticia = 2', callback);
+    };
+
+    this.salvarNoticia = function (noticia, connection, callback) {
+        connection.query('insert into noticias set ?', noticia, callback);
     };
 
     return this;
--- config/server.js
+++ config/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var consign = require('consign');
+var bodyParser = require('body-parser');
 
 var app = express();
 app.set('view engine', 'ejs');
@@ -7,6 +8,9 @@ app.set('view engine', 'ejs');
 // definição do caminho base das views
 // como inclusão do módulo 'server' é feita no app, então o caminho é relativo ao 'app.js'
 app.set('views', './app/views');
+
+// configuração do middleware body-parser para tratar urls codificadas
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // utilizando o Consign para mapear as views no Express de forma rápida
 consign()
